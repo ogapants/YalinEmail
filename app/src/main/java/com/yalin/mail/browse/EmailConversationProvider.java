969d7eec01a2fa47e649b47f0611e0ad6458037b
jinyalin <jinyalin@baidu.com>
2017-07-12 13:25:15 +0800

    add app logic

---
+++ app/src/main/java/com/yalin/mail/browse/EmailConversationProvider.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yalin.mail.browse;
+
+import com.android.mail.browse.ConversationCursor.ConversationProvider;
+import com.yalin.email.R;
+
+public class EmailConversationProvider extends ConversationProvider {
+    // The authority of our conversation provider (a forwarding provider)
+    // This string must match the declaration in AndroidManifest.xml
+    private static String sAuthority;
+
+    @Override
+    protected String getAuthority() {
+        if (sAuthority == null) {
+            sAuthority = getContext().getString(R.string.authority_conversation_provider);
+        }
+        return sAuthority;
+    }
+}
